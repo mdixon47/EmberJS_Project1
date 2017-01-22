ab120af104767da71f298de7c81f150accf79772
Malik Dixon <mdixon@rap.midco.net>
2017-01-21 22:28:39 -0700

    first commit

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'emtasks',
+    podModulePrefix: 'emtasks/pods',
     environment: environment,
     rootURL: '/',
     locationType: 'auto',
@@ -24,9 +25,9 @@ module.exports = function(environment) {
   };
 
   if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
+     ENV.APP.LOG_RESOLVER = true;
     // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
+     ENV.APP.LOG_TRANSITIONS = true;
     // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
     // ENV.APP.LOG_VIEW_LOOKUPS = true;
   }
